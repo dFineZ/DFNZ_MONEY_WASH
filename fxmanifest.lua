599354ff562aedc687179716a826529d8abdae78
DFNZ Scripts <122015772+dFineZ@users.noreply.github.com>
2023-09-22 19:08:13 +0200

    Add files via upload
    version 1.0.0

---
+++ fxmanifest.lua
@@ -0,0 +1,22 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'DFNZ'
+description 'simple moneywash with some animations'
+version '1.0.0'
+
+shared_script {
+    '@es_extended/imports.lua',
+    '@ox_lib/init.lua'
+}
+
+client_scripts {
+    "config.lua",
+    "client/client.lua",
+}
+
+server_scripts {
+    "config.lua",
+    "server/server.lua",
+}
